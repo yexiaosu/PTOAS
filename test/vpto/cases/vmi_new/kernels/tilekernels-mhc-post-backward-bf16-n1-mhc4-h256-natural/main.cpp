0cfc03cd2fe0eac6e1a09737b4c2043d5fcf2ea4
yexiaosu <wang.lan313@sjtu.edu.cn>
2026-08-20 18:34:00 +0800

    test(vpto): add TileKernels VMI runtime fixtures

---
+++ test/vpto/cases/vmi_new/kernels/tilekernels-mhc-post-backward-bf16-n1-mhc4-h256-natural/main.cpp
@@ -0,0 +1,182 @@
+// Copyright (c) 2026 Huawei Technologies Co., Ltd.
+// This program is free software, you can redistribute it and/or modify it under the terms and conditions of
+// CANN Open Software License Agreement Version 2.0 (the "License").
+// Please refer to the License for details. You may not use this file except in compliance with the License.
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+// See LICENSE in the root of the software repository for the full text of the License.
+
+#include "acl/acl.h"
+#include "test_common.h"
+#include <climits>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace PtoTestCommon;
+
+#define ACL_CHECK(expr)                                                        \
+  do {                                                                         \
+    const aclError _ret = (expr);                                              \
+    if (_ret != ACL_SUCCESS) {                                                 \
+      std::fprintf(stderr, "[ERROR] %s failed: %d (%s:%d)\n", #expr,          \
+                   (int)_ret, __FILE__, __LINE__);                             \
+      return 1;                                                                \
+    }                                                                          \
+  } while (0)
+
+void LaunchVmi_tilekernels_mhc_post_backward_bf16_n1_mhc4_h256_natural_kernel(
+    uint16_t *, float *, uint16_t *, float *, uint16_t *, float *, uint16_t *,
+    float *, uint16_t *, void *);
+
+int main() {
+  constexpr size_t kDOutBytes = 2048;
+  constexpr size_t kCombBytes = 64;
+  constexpr size_t kResidualBytes = 2048;
+  constexpr size_t kPostBytes = 32;
+  constexpr size_t kPostOutputBytes = 16;
+  constexpr size_t kXBytes = 512;
+  uint16_t *dOutHost = nullptr;
+  uint16_t *residualHost = nullptr;
+  uint16_t *xHost = nullptr;
+  uint16_t *dResidualHost = nullptr;
+  uint16_t *dXHost = nullptr;
+  float *combHost = nullptr;
+  float *postHost = nullptr;
+  float *dCombHost = nullptr;
+  float *dPostHost = nullptr;
+  uint16_t *dOutDevice = nullptr;
+  uint16_t *residualDevice = nullptr;
+  uint16_t *xDevice = nullptr;
+  uint16_t *dResidualDevice = nullptr;
+  uint16_t *dXDevice = nullptr;
+  float *combDevice = nullptr;
+  float *postDevice = nullptr;
+  float *dCombDevice = nullptr;
+  float *dPostDevice = nullptr;
+  int rc = 0;
+  bool aclInited = false;
+  bool deviceSet = false;
+  int deviceId = 0;
+  aclrtStream stream = nullptr;
+
+  ACL_CHECK(aclInit(nullptr));
+  aclInited = true;
+  if (const char *envDevice = std::getenv("ACL_DEVICE_ID")) {
+    char *end = nullptr;
+    const long parsedDevice = std::strtol(envDevice, &end, 10);
+    if (*envDevice == '\0' || *end != '\0' || parsedDevice < 0 ||
+        parsedDevice > INT_MAX) {
+      std::fprintf(stderr, "[ERROR] invalid ACL_DEVICE_ID: %s\n", envDevice);
+      return 1;
+    }
+    deviceId = static_cast<int>(parsedDevice);
+  }
+  ACL_CHECK(aclrtSetDevice(deviceId));
+  deviceSet = true;
+  ACL_CHECK(aclrtCreateStream(&stream));
+
+  ACL_CHECK(aclrtMallocHost((void **)(&dOutHost), kDOutBytes));
+  ACL_CHECK(aclrtMallocHost((void **)(&combHost), kCombBytes));
+  ACL_CHECK(aclrtMallocHost((void **)(&residualHost), kResidualBytes));
+  ACL_CHECK(aclrtMallocHost((void **)(&postHost), kPostBytes));
+  ACL_CHECK(aclrtMallocHost((void **)(&xHost), kXBytes));
+  ACL_CHECK(aclrtMallocHost((void **)(&dCombHost), kCombBytes));
+  ACL_CHECK(aclrtMallocHost((void **)(&dResidualHost), kResidualBytes));
+  ACL_CHECK(aclrtMallocHost((void **)(&dPostHost), kPostBytes));
+  ACL_CHECK(aclrtMallocHost((void **)(&dXHost), kXBytes));
+
+  ACL_CHECK(aclrtMalloc((void **)&dOutDevice, kDOutBytes,
+                        ACL_MEM_MALLOC_HUGE_FIRST));
+  ACL_CHECK(aclrtMalloc((void **)&combDevice, kCombBytes,
+                        ACL_MEM_MALLOC_HUGE_FIRST));
+  ACL_CHECK(aclrtMalloc((void **)&residualDevice, kResidualBytes,
+                        ACL_MEM_MALLOC_HUGE_FIRST));
+  ACL_CHECK(aclrtMalloc((void **)&postDevice, kPostBytes,
+                        ACL_MEM_MALLOC_HUGE_FIRST));
+  ACL_CHECK(aclrtMalloc((void **)&xDevice, kXBytes,
+                        ACL_MEM_MALLOC_HUGE_FIRST));
+  ACL_CHECK(aclrtMalloc((void **)&dCombDevice, kCombBytes,
+                        ACL_MEM_MALLOC_HUGE_FIRST));
+  ACL_CHECK(aclrtMalloc((void **)&dResidualDevice, kResidualBytes,
+                        ACL_MEM_MALLOC_HUGE_FIRST));
+  ACL_CHECK(aclrtMalloc((void **)&dPostDevice, kPostBytes,
+                        ACL_MEM_MALLOC_HUGE_FIRST));
+  ACL_CHECK(aclrtMalloc((void **)&dXDevice, kXBytes,
+                        ACL_MEM_MALLOC_HUGE_FIRST));
+
+  ReadFile("./v1.bin", kDOutBytes, dOutHost, kDOutBytes);
+  ReadFile("./v2.bin", kCombBytes, combHost, kCombBytes);
+  ReadFile("./v3.bin", kResidualBytes, residualHost, kResidualBytes);
+  ReadFile("./v4.bin", kPostBytes, postHost, kPostBytes);
+  ReadFile("./v5.bin", kXBytes, xHost, kXBytes);
+  ReadFile("./v6.bin", kCombBytes, dCombHost, kCombBytes);
+  ReadFile("./v7.bin", kResidualBytes, dResidualHost, kResidualBytes);
+  ReadFile("./v8.bin", kPostBytes, dPostHost, kPostBytes);
+  ReadFile("./v9.bin", kXBytes, dXHost, kXBytes);
+
+  ACL_CHECK(aclrtMemcpy(dOutDevice, kDOutBytes, dOutHost, kDOutBytes,
+                       ACL_MEMCPY_HOST_TO_DEVICE));
+  ACL_CHECK(aclrtMemcpy(combDevice, kCombBytes, combHost, kCombBytes,
+                       ACL_MEMCPY_HOST_TO_DEVICE));
+  ACL_CHECK(aclrtMemcpy(residualDevice, kResidualBytes, residualHost,
+                       kResidualBytes, ACL_MEMCPY_HOST_TO_DEVICE));
+  ACL_CHECK(aclrtMemcpy(postDevice, kPostBytes, postHost, kPostBytes,
+                       ACL_MEMCPY_HOST_TO_DEVICE));
+  ACL_CHECK(aclrtMemcpy(xDevice, kXBytes, xHost, kXBytes,
+                       ACL_MEMCPY_HOST_TO_DEVICE));
+  ACL_CHECK(aclrtMemcpy(dCombDevice, kCombBytes, dCombHost, kCombBytes,
+                       ACL_MEMCPY_HOST_TO_DEVICE));
+  ACL_CHECK(aclrtMemcpy(dResidualDevice, kResidualBytes, dResidualHost,
+                       kResidualBytes, ACL_MEMCPY_HOST_TO_DEVICE));
+  ACL_CHECK(aclrtMemcpy(dPostDevice, kPostBytes, dPostHost, kPostBytes,
+                       ACL_MEMCPY_HOST_TO_DEVICE));
+  ACL_CHECK(aclrtMemcpy(dXDevice, kXBytes, dXHost, kXBytes,
+                       ACL_MEMCPY_HOST_TO_DEVICE));
+
+  LaunchVmi_tilekernels_mhc_post_backward_bf16_n1_mhc4_h256_natural_kernel(
+      dOutDevice, combDevice, residualDevice, postDevice, xDevice, dCombDevice,
+      dResidualDevice, dPostDevice, dXDevice, stream);
+  ACL_CHECK(aclrtSynchronizeStream(stream));
+  ACL_CHECK(aclrtMemcpy(dCombHost, kCombBytes, dCombDevice, kCombBytes,
+                       ACL_MEMCPY_DEVICE_TO_HOST));
+  ACL_CHECK(aclrtMemcpy(dResidualHost, kResidualBytes, dResidualDevice,
+                       kResidualBytes, ACL_MEMCPY_DEVICE_TO_HOST));
+  ACL_CHECK(aclrtMemcpy(dPostHost, kPostBytes, dPostDevice, kPostBytes,
+                       ACL_MEMCPY_DEVICE_TO_HOST));
+  ACL_CHECK(aclrtMemcpy(dXHost, kXBytes, dXDevice, kXBytes,
+                       ACL_MEMCPY_DEVICE_TO_HOST));
+  WriteFile("./v6.bin", dCombHost, kCombBytes);
+  WriteFile("./v7.bin", dResidualHost, kResidualBytes);
+  WriteFile("./v8.bin", dPostHost, kPostOutputBytes);
+  WriteFile("./v9.bin", dXHost, kXBytes);
+
+  aclrtFree(dOutDevice);
+  aclrtFree(combDevice);
+  aclrtFree(residualDevice);
+  aclrtFree(postDevice);
+  aclrtFree(xDevice);
+  aclrtFree(dCombDevice);
+  aclrtFree(dResidualDevice);
+  aclrtFree(dPostDevice);
+  aclrtFree(dXDevice);
+  aclrtFreeHost(dOutHost);
+  aclrtFreeHost(combHost);
+  aclrtFreeHost(residualHost);
+  aclrtFreeHost(postHost);
+  aclrtFreeHost(xHost);
+  aclrtFreeHost(dCombHost);
+  aclrtFreeHost(dResidualHost);
+  aclrtFreeHost(dPostHost);
+  aclrtFreeHost(dXHost);
+  if (stream) {
+    aclrtDestroyStream(stream);
+  }
+  if (deviceSet) {
+    aclrtResetDevice(deviceId);
+  }
+  if (aclInited) {
+    aclFinalize();
+  }
+  return rc;
+}
